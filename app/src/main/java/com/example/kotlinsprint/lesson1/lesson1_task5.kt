911be30fe1b5650c601cc800c9af80f0043aeec5
niya11061979 <poyas_oriona@rambler.ru>
2023-04-02 13:46:11 +0300

    solution lesson1 task5

---
+++ app/src/main/java/com/example/kotlinsprint/lesson1/lesson1_task5.kt
@@ -0,0 +1,23 @@
+package com.example.kotlinsprint.lesson1
+
+fun main() {
+    print("введите количество секунд ,которые Гагарин провел в космосе :")
+    val secondsOfFlight = readLine()?.toIntOrNull() ?: return
+    val minAndSec = secondsToMinutes(secondsOfFlight)
+    println("Время проведенное в космос(мм:сс) $minAndSec")
+
+    if (minAndSec == "108:00") {
+        println("Отлично вы хорошо знаете историю")
+    } else {
+        println("Вам нужно подтянуть знания , Ю.Гагарина провел в космосе 108 мин 00 секунд")
+    }
+}
+
+fun secondsToMinutes(secondsOfFlight: Int): String {
+    val min = secondsOfFlight / 60
+    val sec = secondsOfFlight % 60
+    val minStr = if (min < 10) "0$min" else min
+    val secStr = if (sec < 10) "0$sec" else sec
+
+    return "$minStr:$secStr"
+}
